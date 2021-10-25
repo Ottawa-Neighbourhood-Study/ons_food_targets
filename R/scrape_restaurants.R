28761913e26cae66e289025823d8abfca6ce4544
Christopher Belanger <christopher.a.belanger@gmail.com>
2021-10-25 14:52:55 -0400

    added mcdonald's

--- R/scrape_restaurants.R
+++ R/scrape_restaurants.R
@@ -13,6 +13,43 @@ fastfood_chains <- old_food %>%
 
 #harveys <- read_csv("data/restaurants/harveys.csv")
 
+
+scrape_mcdonalds <- function(){
+  
+  # get up to 250 mcdonalds within 100km of ottawa
+  results <- httr::GET("https://www.mcdonalds.com/googleappsv2/geolocation?latitude=45.4215296&longitude=-75.69719309999999&radius=100&maxResults=250&country=ca&language=en-ca&showClosed=&hours24Text=Open%2024%20hr")
+  
+  mcd <- results %>%
+    httr::content()
+  
+  mcdonalds <- mcd[[1]] %>%
+    purrr::map_dfr(function(x) {
+      lat <- x$geometry$coordinates[[1]]
+      lon <- x$geometry$coordinates[[2]]
+      
+      ad1 <- x$properties$addressLine1
+      ad2 <- x$properties$addressLine2
+      ad3 <- x$properties$addressLine3
+      ad4 <- x$properties$addressLine4
+      postcode <- x$properties$postcode
+      telephone <- if_else(is.null(x$properties$telephone), "", x$properties$telephone)
+      
+      if (is.null(ad2)) address <- sprintf("%s, %s, %s, %s", ad1, ad3, postcode, ad4)
+      if (!is.null(ad2)) address <- sprintf("%s, %s, %s, %s, %s", ad1, ad2, ad3, postcode, ad4)
+      
+      
+      tibble(name = "McDonald's",
+             address = address,
+             phone = telephone,
+             lat = lat, lon = lon)
+    })
+  
+  write_csv(mcdonalds, "data/restaurants/mcdonalds.csv")
+  
+  return(mcdonalds)
+  
+}
+
 scrape_subway <- function(){
   
   # we can only get 50 results at once, so just do a one-page 50-item search
@@ -97,34 +134,34 @@ scrape_tim_hortons <- function(){
 "query":"query GetRestaurants($input: RestaurantsInput) {   restaurants(input: $input) {     pageInfo {       hasNextPage       endCursor       __typename     }     totalCount     nodes {       ...RestaurantNodeFragment       __typename     }     __typename   } }  fragment RestaurantNodeFragment on RestaurantNode {   _id   storeId   isAvailable   posVendor   chaseMerchantId   curbsideHours {     ...OperatingHoursFragment     __typename   }   deliveryHours {     ...OperatingHoursFragment     __typename   }   diningRoomHours {     ...OperatingHoursFragment     __typename   }   distanceInMiles   drinkStationType   driveThruHours {     ...OperatingHoursFragment     __typename   }   driveThruLaneType   email   environment   franchiseGroupId   franchiseGroupName   frontCounterClosed   hasBreakfast   hasBurgersForBreakfast   hasCatering   hasCurbside   hasDelivery   hasDineIn   hasDriveThru   hasMobileOrdering   hasParking   hasPlayground   hasTakeOut   hasWifi   id   isDarkKitchen   isFavorite   isRecent   latitude   longitude   mobileOrderingStatus   name   number   parkingType   phoneNumber   physicalAddress {     address1     address2     city     country     postalCode     stateProvince     stateProvinceShort     __typename   }   playgroundType   pos {     vendor     __typename   }   posRestaurantId   restaurantImage {     asset {       _id       metadata {         lqip         palette {           dominant {             background             foreground             __typename           }           __typename         }         __typename       }       __typename     }     crop {       top       bottom       left       right       __typename     }     hotspot {       height       width       x       y       __typename     }     __typename   }   restaurantPosData {     _id     __typename   }   status   vatNumber   __typename }  fragment OperatingHoursFragment on OperatingHours {   friClose   friOpen   monClose   monOpen   satClose   satOpen   sunClose   sunOpen   thrClose   thrOpen   tueClose   tueOpen   wedClose   wedOpen   __typename } "}]'
   
   
-#[{"operationName":"GetRestaurants","variables":{"input":{"filter":"NEARBY","coordinates":{"userLat":45.4215296,"userLng":-75.69719309999999,"searchRadius":80000},"first":500,"status":"OPEN"}},"query":"query GetRestaurants($input: RestaurantsInput) {\n  restaurants(input: $input) {\n    pageInfo {\n      hasNextPage\n      endCursor\n      __typename\n    }\n    totalCount\n    nodes {\n      ...RestaurantNodeFragment\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment RestaurantNodeFragment on RestaurantNode {\n  _id\n  storeId\n  isAvailable\n  posVendor\n  chaseMerchantId\n  curbsideHours {\n    ...OperatingHoursFragment\n    __typename\n  }\n  deliveryHours {\n    ...OperatingHoursFragment\n    __typename\n  }\n  diningRoomHours {\n    ...OperatingHoursFragment\n    __typename\n  }\n  distanceInMiles\n  drinkStationType\n  driveThruHours {\n    ...OperatingHoursFragment\n    __typename\n  }\n  driveThruLaneType\n  email\n  environment\n  franchiseGroupId\n  franchiseGroupName\n  frontCounterClosed\n  hasBreakfast\n  hasBurgersForBreakfast\n  hasCatering\n  hasCurbside\n  hasDelivery\n  hasDineIn\n  hasDriveThru\n  hasMobileOrdering\n  hasParking\n  hasPlayground\n  hasTakeOut\n  hasWifi\n  id\n  isDarkKitchen\n  isFavorite\n  isRecent\n  latitude\n  longitude\n  mobileOrderingStatus\n  name\n  number\n  parkingType\n  phoneNumber\n  physicalAddress {\n    address1\n    address2\n    city\n    country\n    postalCode\n    stateProvince\n    stateProvinceShort\n    __typename\n  }\n  playgroundType\n  pos {\n    vendor\n    __typename\n  }\n  posRestaurantId\n  restaurantImage {\n    asset {\n      _id\n      metadata {\n        lqip\n        palette {\n          dominant {\n            background\n            foreground\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    crop {\n      top\n      bottom\n      left\n      right\n      __typename\n    }\n    hotspot {\n      height\n      width\n      x\n      y\n      __typename\n    }\n    __typename\n  }\n  restaurantPosData {\n    _id\n    __typename\n  }\n  status\n  vatNumber\n  __typename\n}\n\nfragment OperatingHoursFragment on OperatingHours {\n  friClose\n  friOpen\n  monClose\n  monOpen\n  satClose\n  satOpen\n  sunClose\n  sunOpen\n  thrClose\n  thrOpen\n  tueClose\n  tueOpen\n  wedClose\n  wedOpen\n  __typename\n}\n"}]  
+  #[{"operationName":"GetRestaurants","variables":{"input":{"filter":"NEARBY","coordinates":{"userLat":45.4215296,"userLng":-75.69719309999999,"searchRadius":80000},"first":500,"status":"OPEN"}},"query":"query GetRestaurants($input: RestaurantsInput) {\n  restaurants(input: $input) {\n    pageInfo {\n      hasNextPage\n      endCursor\n      __typename\n    }\n    totalCount\n    nodes {\n      ...RestaurantNodeFragment\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment RestaurantNodeFragment on RestaurantNode {\n  _id\n  storeId\n  isAvailable\n  posVendor\n  chaseMerchantId\n  curbsideHours {\n    ...OperatingHoursFragment\n    __typename\n  }\n  deliveryHours {\n    ...OperatingHoursFragment\n    __typename\n  }\n  diningRoomHours {\n    ...OperatingHoursFragment\n    __typename\n  }\n  distanceInMiles\n  drinkStationType\n  driveThruHours {\n    ...OperatingHoursFragment\n    __typename\n  }\n  driveThruLaneType\n  email\n  environment\n  franchiseGroupId\n  franchiseGroupName\n  frontCounterClosed\n  hasBreakfast\n  hasBurgersForBreakfast\n  hasCatering\n  hasCurbside\n  hasDelivery\n  hasDineIn\n  hasDriveThru\n  hasMobileOrdering\n  hasParking\n  hasPlayground\n  hasTakeOut\n  hasWifi\n  id\n  isDarkKitchen\n  isFavorite\n  isRecent\n  latitude\n  longitude\n  mobileOrderingStatus\n  name\n  number\n  parkingType\n  phoneNumber\n  physicalAddress {\n    address1\n    address2\n    city\n    country\n    postalCode\n    stateProvince\n    stateProvinceShort\n    __typename\n  }\n  playgroundType\n  pos {\n    vendor\n    __typename\n  }\n  posRestaurantId\n  restaurantImage {\n    asset {\n      _id\n      metadata {\n        lqip\n        palette {\n          dominant {\n            background\n            foreground\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    crop {\n      top\n      bottom\n      left\n      right\n      __typename\n    }\n    hotspot {\n      height\n      width\n      x\n      y\n      __typename\n    }\n    __typename\n  }\n  restaurantPosData {\n    _id\n    __typename\n  }\n  status\n  vatNumber\n  __typename\n}\n\nfragment OperatingHoursFragment on OperatingHours {\n  friClose\n  friOpen\n  monClose\n  monOpen\n  satClose\n  satOpen\n  sunClose\n  sunOpen\n  thrClose\n  thrOpen\n  tueClose\n  tueOpen\n  wedClose\n  wedOpen\n  __typename\n}\n"}]  
   response <- httr::POST(url = url,
                          body = payload
                          ,content_type_json()
-                         )
-
+  )
+  
   r <- response %>%
     httr::content(encoding = "UTF-8")
-    jsonlite::fromJSON()
+  jsonlite::fromJSON()
   
-    stores <- tibble::tibble(data = r[[1]]$data$restaurants$nodes)
-    
-    results <- stores %>%
-      mutate(name = "Tim Horton's", #purrr::map_chr(data, pluck("name")),
-             phone_number = purrr::map_chr(data, pluck("phoneNumber")),
-             address1 = purrr::map_chr(data, pluck, "physicalAddress", "address1"),
-             address2 = purrr::map_chr(data, pluck, "physicalAddress", "address2"),
-             city = purrr::map_chr(data, pluck, "physicalAddress", "city"),
-             province = purrr::map_chr(data, pluck, "physicalAddress", "stateProvinceShort"),
-             postal_code = purrr::map_chr(data, pluck, "physicalAddress", "postalCode"),
-             lat = purrr::map_chr(data, pluck, "latitude"),
-             lng = purrr::map_chr(data, pluck, "longitude"),
-             breakfast_burgers = purrr::map_lgl(data, pluck, "hasBurgersForBreakfast")
-             ) %>%
-      mutate(address = sprintf("%s, %s, %s %s", address1, city, province, postal_code)) %>%
-      select(name, address, phone_number, lat, lng)
-    
-    write_csv(results, "data/restaurants/tim_hortons.csv")
+  stores <- tibble::tibble(data = r[[1]]$data$restaurants$nodes)
+  
+  results <- stores %>%
+    mutate(name = "Tim Horton's", #purrr::map_chr(data, pluck("name")),
+           phone_number = purrr::map_chr(data, pluck("phoneNumber")),
+           address1 = purrr::map_chr(data, pluck, "physicalAddress", "address1"),
+           address2 = purrr::map_chr(data, pluck, "physicalAddress", "address2"),
+           city = purrr::map_chr(data, pluck, "physicalAddress", "city"),
+           province = purrr::map_chr(data, pluck, "physicalAddress", "stateProvinceShort"),
+           postal_code = purrr::map_chr(data, pluck, "physicalAddress", "postalCode"),
+           lat = purrr::map_chr(data, pluck, "latitude"),
+           lng = purrr::map_chr(data, pluck, "longitude"),
+           breakfast_burgers = purrr::map_lgl(data, pluck, "hasBurgersForBreakfast")
+    ) %>%
+    mutate(address = sprintf("%s, %s, %s %s", address1, city, province, postal_code)) %>%
+    select(name, address, phone_number, lat, lng)
+  
+  write_csv(results, "data/restaurants/tim_hortons.csv")
 }
 
 
@@ -163,13 +200,13 @@ scrape_harveys <- function(){
     mutate(address = sprintf("%s %s, %s, %s, %s", streetNumber, street, city, province, postalCode),
            name = "Harvey's") %>%
     select(name, address, phone = phoneNumber)
-    
+  
   df %>%
     write_csv("data/restaurants/harveys.csv") %>%
     return()
   
-# df %>%
-#   sf::st_as_sf(coords = c("longitude", "latitude"), crs = "WGS84") %>% ggplot() + geom_sf()
-    
+  # df %>%
+  #   sf::st_as_sf(coords = c("longitude", "latitude"), crs = "WGS84") %>% ggplot() + geom_sf()
+  
   
 }
